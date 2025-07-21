39058df3b5512e4c621ced7d21907fdb3c727f44
Suraj Shaw <surajshaw1711@gmail.com>
2025-07-21 07:26:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { writeFileSync } from "fs";
+
+// Write CNAME to dist before deploy
+writeFileSync("dist/CNAME", "www.maetrixs.com");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
